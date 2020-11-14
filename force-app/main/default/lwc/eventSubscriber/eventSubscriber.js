8735d0c9e9c6b6bb3fa1ce769e570d56b49a7bca
verooonnika <veronikamail.21@gmail.com>
2020-11-14 18:08:21 +0300

    feat: add account Id check

--- force-app/main/default/lwc/eventSubscriber/eventSubscriber.js
+++ force-app/main/default/lwc/eventSubscriber/eventSubscriber.js
@@ -1,16 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api} from 'lwc';
 import { subscribe, unsubscribe, onError } from 'lightning/empApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class eventSubscriber extends LightningElement {
     channelName = '/event/Account_Change__e';
     subscription = {};
+    @api recordId;
 
     connectedCallback() {    
        
         const messageCallback = (response) => {
             var message = response['data']['payload']['Message__c'];
-            this.showToast(message);
+            var eventRecordId = response['data']['payload']['Record_Id__c'];
+
+            if(eventRecordId == this.recordId){
+                this.showToast(message);
+            }
         };
 
         subscribe(this.channelName, -1, messageCallback).then(response => {
